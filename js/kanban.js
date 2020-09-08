fc8b5d9beebb2d9c06d89fdf8b3ad55e7613f4de
Waqar Ahmad <WAAH@cmck.com>
2020-09-08 14:29:37 +0100

    completed module

--- js/kanban.js
+++ js/kanban.js
@@ -1,3 +1,4 @@
+
 let order = 1;
 let adding = false;
 
@@ -16,7 +17,75 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+
+  let item = document.createElement('div');
+
+  item.classList.add('item');
+  item.id = 'item-' + order;
+  item.draggable = true;
+
+  item.addEventListener('dragstart', event => {
+
+    return event.dataTransfer.setData(event.target.id);
+
+  });
+
+  item.addEventListener('dragend', (event) => {
+
+    return event.dataTransfer.clearData();
+
+  });
+
+
+  let input = document.createElement('input');
+  input.append(item);
+
+  let save_btn = document.createElement('button');
+  save_btn.innerHTML('Save');
+
+  save_btn.addEventListener('click', () =>{
+
+    save_btn.error('');
+
+    if(input.value != ''){
+
+      order += 1;
+
+      item.value = input;
+
+      adding = false;
+
+    } else {
+      save_btn.error(message);
+    }
+
+    save_btn.append(item);
+
+    item.create_item();
+
+
+
+  });
+
 };
 
 document.querySelectorAll('.drop').forEach(element => {
+
+  element.addEventListener('drop',(event) => {
+
+    event.preventDefault();
+    
+    const id = event.dataTransfer.getData(); 
+ 
+  event.target.append(document.getElementById('id'));
+     
+  element.addEventListener('dragover',(event) => {
+
+
+  });
+
+  });
+
+
+
 });
